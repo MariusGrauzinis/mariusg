c044061b34ff30897ac476b1a7bab041d6ecda5e
Mariusgrauzinis <mariusgrauzinis@yahoo.co.uk>
2025-02-08 13:20:29 +0200

    new

---
+++ pamoka1/main2.py
@@ -0,0 +1,4 @@
+
+restaurants = [{"name": "Vytautas", "rating": 3.0}, {"name": "Mindaugas", "rating": 10.0}, {"name": "Another_one", "rating": 9.0}]
+
+
---
+++ pamoka1/nesamones.py
@@ -0,0 +1,24 @@
+# sukuriam tuscia listą su restoranais: restaurants = []
+# leidziam useriui ivesti kiek restoranu sukurti? (skaicius int)
+# iteruojam per tą skaičių ir:
+#     prasom name + rating
+#     sukuriam iš jų diciotnary
+#     restaurants.append(new_restaurant)
+
+restaurants = [{"name": "Vytautas", "rating": 3.0}, {"name": "Mindaugas", "rating": 10.0}, {"name": "Another_one", "rating": 9.0}]
+
+
+minimum_threshold = input("enter minimum threshold: ")
+# check if user entered anything at all?:
+#    set minimum_therhsold and convert to float
+# else:  use default  4.0
+
+
+# iterate through restaurants:
+#    check if restaurant["rating"] >= minimum_therhsold
+
+restaurants = [{"name": "Mindaugas", "rating": 10.0}, {"name": "Another_one", "rating": 9.0}]
+
+# sort the restaurants by name
+
+# print the result
