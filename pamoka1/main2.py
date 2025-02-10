0fabd08aa671a375fabb8abc4d9ebe7b9bc69660
Mariusgrauzinis <mariusgrauzinis@yahoo.co.uk>
2025-02-10 20:05:08 +0200

    new

--- pamoka1/main2.py
+++ pamoka1/main2.py
@@ -1,31 +1,23 @@
+def fitered_list(rest_list: list[dict[str, str | float]], min_search_rating: float) -> list[str]:
+    for entry in rest_list:
+        if entry["rating"] >= min_search_rating:
+            filtered_list.append(entry["name"])
+    return filtered_list
 
-# restaurants = [
-#     {"name": "CiliPica", "rating": 4.5},
-#     {"name": "SushiExpress", "rating": 3.8},
-#     {"name": "BurgerKing", "rating": 4.2},
-#     {"name": "DominoPizza", "rating": 4.7},
-#     {"name": "ExpresPica", "rating": 3.5},
-#     {"name": "KFC", "rating": 4.1},
-#     {"name": "FelixChiken", "rating": 3.5},
-#     {"name": "WokBusters", "rating": 4.9}
-# ]
 
-restaurant_list = []
-restaurants_count = int(input("Enter number of restaurants"))
-for i in range(restaurants_count):
-    restaurant_name = input("Restaurant name:")
-    restaurant_rating = float(input("Restaurant rating:"))
-    restaurant_list.append({"name": restaurant_name, "rating": restaurant_list})
-searching_rating = input("Enter minimum rating")
-if searching_rating.isnumeric():
-    searching_rating = float(searching_rating)
+filtered_list = []
+rest_list = []
+rest_count = int(input("Enter the number of restorants:" ))
+for i in range(rest_count):
+    rest_name = input("Restorant name: ")
+    rest_rating = float(input("Restorant rating "))
+    rest_list.append({"name": rest_name, "rating": rest_rating}) 
+min_search_rating = float(input("Enter the minimum rating: "))
+if min_search_rating.isnumeric():
+    min_search_rating = float(min_search_rating)
 else:
-    searching_rating = 4.0
-def filtered_list(restaurant_list, searching_rating):
-    filtered_list = []
-    for r in restaurant_list:
-        if r["rating"] <= searching_rating:
-            filtered_list.append(r"name")
-        return filtered_list
-result = filtered_list(restaurant_list, searching_rating)
-print(result)
+    min_search_rating = 4.0    
+resul = fitered_list(rest_list, min_search_rating)
+resul.sort()
+print(resul)
+ 
